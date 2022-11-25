29ab66404ddc0c7eb4c2e7e4d81d15f79f3e8a72
equitysoft <>
2022-11-25 18:50:30 +0530

    javascript
    variable define, datatypes, if else condition, nested if , while loop , mathematical operation ( operator )

---
+++ basic.js
@@ -0,0 +1,102 @@
+" use strict ";
+
+var firstname = "Abhisha";
+
+// console.log(firstname);
+
+let lastName = "patel";
+
+lastName = lastName.toUpperCase();
+
+// console.log(lastName);
+
+var add = 50 ;
+// console.log(add + 10);
+
+// nested if
+
+// let winingnuber = 26;
+
+// let userguessnumber = +prompt("guess number");
+
+// if(winingnuber === userguessnumber){
+
+//     console.log("your guess number is right");
+
+// }else{
+//     if(userguessnumber < winingnuber){
+
+//         console.log("too low");
+
+//     }else{
+//         console.log("too high");
+//     }
+// }
+
+// let tempInDegree = 26 ;
+
+// if(tempInDegree < 0){
+//     console.log("extremely cold outside");
+// }else if(tempInDegree < 16){
+//     console.log("it is cold outside");
+// }else if(tempInDegree < 25){
+//     console.log("wheather is okay");
+// }else if(tempInDegree < 35){
+//     console.log("lets go for swim");
+// }else if(tempInDegree < 45){
+//     console.log("turn on AC")
+// }else{
+//     console.log("too hot!!");
+// }
+
+// let day = 0;
+
+// switch(day){
+
+//     case 0:
+//         console.log("sunday");
+//         break;
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("thursday");
+//         break;
+//     case 5:
+//         console.log("friday");
+//         break;
+//     case 6:
+//         console.log("saturday");
+//         break;
+
+// }
+
+// let i = 0;
+
+// while(i<=9){
+//     console.log(i);
+//     i++;
+// }
+// console.log(`current value of i is ${i}`)
+
+// let num = 100;
+// let total = 0; 
+// let i = 0 ;
+
+// while(i<=100){
+//     total = total + i;
+//     i++;
+// }
+// console.log(total); 
+
+// let num = 1000;
+
+// let total = (num*(num+1))/2;
+// console.log(total);
+
